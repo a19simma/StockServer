896284893a58573cd76d20ec5a3658461e39e155
a19simma <a19simma@student.his.se>
2022-06-28 17:28:00 +0200

    uwsgi setup complete and working with docker. For final deployment, remove stat server and add nginx ingress

--- src/app.py
+++ src/app.py
@@ -1,9 +1,18 @@
 from flask import Flask
 
-from src.routes import ticker
+from src.routes import ticker, company
 
 app = Flask(__name__)
 app.register_blueprint(ticker.ticker)
+app.register_blueprint(company.company)
+
+
+@app.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
+
+
+application = app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(threaded=True, port="3000", host="0.0.0.0")
--- src/routes/ticker.py
+++ src/routes/ticker.py
@@ -10,10 +10,13 @@ from src.model.tables import StocksDaily
 
 ticker = Blueprint('ticker', __name__)
 
-@ticker.route('/ticker/<symbol>', methods=['GET', 'POST'])
-def getTicker(symbol):
-    data = session.query(StocksDaily).all()
-    data = [ x.__dict__ for x in data ]
-    data = jsonify(data)
-    return data
-    
+
+@ticker.route('/ticker/<ticker>', methods=['GET', 'POST'])
+def getTicker(ticker):
+    stmt = select(StocksDaily).where(StocksDaily.ticker == ticker)
+    data = session.execute(stmt)
+    result = {ticker: {}}
+    for obj in data.scalars():
+        result[obj.ticker][str(obj.date)] = {'open': obj.open, 'high': obj.high,
+                                             'low': obj.low, 'close': obj.close, 'volume': obj.volume, }
+    return result
