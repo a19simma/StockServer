daa2e3973ce19b8de84f2f08439139c90813298c
a19simma <a19simma@student.his.se>
2022-08-13 18:27:29 +0200

    added cors

--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from src.routes import ticker, company
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(ticker.ticker)
 app.register_blueprint(company.company)
 
