8cf21f79e3964da0ec985ed80257af43198d1e2e
a19simma <a19simma@student.his.se>
2022-08-25 13:35:27 +0200

    bugfixes in update script

--- src/database/update.py
+++ src/database/update.py
@@ -1,23 +1,32 @@
 from datetime import datetime, timedelta
 import pandas as pd
+import asyncio
 
-from src.model.tables import Company
+from src.model.tables import Company, StocksDaily
 from src.database.connection import Session
 from src.data_sources.yahoo import YahooFinance
+
+
+async def updateTicker(ticker, updated, now):
+    data = await yf.getTicker(ticker, round(updated.timestamp()), round(now))
+    session.query(Company)\
+        .filter(Company.ticker == ticker)\
+        .update({Company.updated: datetime.fromtimestamp(now)}, synchronize_session=False)
+    return data
+
 
 if __name__ == '__main__':
     yf = YahooFinance()
     with Session() as session:
         tickers = session.query(Company.ticker, Company.updated).all()
-        now = datetime.now()
-        data = pd.DataFrame()
+        now = datetime.now().timestamp()
+        data = []
         for ticker, updated in tickers:
             if datetime.now() - updated > timedelta(hours=18):
-                data.concat(yf.getTicker(ticker, updated, now),
-                            ignore_index=True)
-                session.query(Company)\
-                    .filter(Company.ticker == ticker)\
-                    .update(updated=now)\
-                    .execution_options(synchronize_session="fetch")
-        company = Company()
-        company.addDataFrame(data, session)
+                ticker = asyncio.run(updateTicker(
+                    ticker, updated, now))
+                data.append(ticker)
+        data = pd.concat(data)
+        stocksdaily = StocksDaily()
+        stocksdaily.addDataFrame(data, session)
+        session.commit()
